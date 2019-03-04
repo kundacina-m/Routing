fd0d15241308bc39eb2a6be07bce62b8f74f1481
Milan Kundacina <milankundacina@uns.ac.rs>
2019-03-04 16:11:18 +0100

    Connected navigation with bottom bar, navigation now fully functional

--- app/src/main/java/com/example/topnews/FrameActivity.kt
+++ app/src/main/java/com/example/topnews/FrameActivity.kt
@@ -2,11 +2,22 @@ package com.example.topnews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_frame.*
 
 class FrameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame)
+
+        val navCtrl = Navigation.findNavController(this,R.id.nav_host_fragment)
+        setupBottomNavBar(navCtrl)
+    }
+
+    private fun setupBottomNavBar(navCtrl: NavController){
+        bottom_navigation.setupWithNavController(navCtrl)
     }
 }
