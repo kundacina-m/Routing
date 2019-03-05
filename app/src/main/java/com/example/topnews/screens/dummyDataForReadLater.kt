a3a55d0da272e76bdb28c8ef4444f665c6753f55
Milan Kundacina <milankundacina@uns.ac.rs>
2019-03-05 15:26:50 +0100

    Added dummy data for testing purposes when feature is finished

---
+++ app/src/main/java/com/example/topnews/screens/dummyDataForReadLater.kt
@@ -0,0 +1,24 @@
+package com.example.topnews.screens
+
+data class dummyDataForReadLater(val title: String, val image: String,val source:String)
+object fakeData {
+    val fakeData = mutableListOf<dummyDataForReadLater>(
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/images/ic/1024x576/p072gvqq.jpg","BBC"),
+        dummyDataForReadLater("aaa", "https://ichef.bbci.co.uk/images/ic/1024x576/p072gvqq.jpg","CNN"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/images/ic/1024x576/p072gvqq.jpg","BBC"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/1279D/production/_105877657_052601306.jpg","RTS"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/8F45/production/_105877663_18317fdb-672b-4783-9683-2205395a5446.jpg","ATV"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/4F5C/production/_105861302_p072cczb.jpg","BIH"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","Herceg"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/B781/production/_105877964_052727162.jpg","sth"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","Hisoty"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","hooo"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","heeeee"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","hiiii"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","howhow"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","nebe"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","eewr"),
+        dummyDataForReadLater("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "https://ichef.bbci.co.uk/news/1024/branded_news/14FD9/production/_105877958_052707307.jpg","rewre")
+
+    )
+}
