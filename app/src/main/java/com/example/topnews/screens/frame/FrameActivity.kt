db1c47ded8e94ab507577200b073ea34c5ea1d16
Milan Kundacina <milankundacina@uns.ac.rs>
2019-03-05 17:38:30 +0100

    Inflated search in menu, implemented navigation through expand and collapse

--- app/src/main/java/com/example/topnews/screens/frame/FrameActivity.kt
+++ app/src/main/java/com/example/topnews/screens/frame/FrameActivity.kt
@@ -2,6 +2,9 @@ package com.example.topnews.screens.frame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.MenuItemCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
@@ -10,15 +13,43 @@ import kotlinx.android.synthetic.main.activity_frame.*
 
 class FrameActivity : AppCompatActivity() {
 
+    private lateinit var navCtrl: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frame)
 
-        val navCtrl = Navigation.findNavController(this, R.id.nav_host_fragment)
-        setupBottomNavBar(navCtrl)
+        navCtrl = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupBottomNavBar()
     }
 
-    private fun setupBottomNavBar(navCtrl: NavController){
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+
+        val searchItem = menu!!.findItem(R.id.search)
+        defineSearchItemBehaviour(searchItem)
+
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    private fun defineSearchItemBehaviour(searchItem: MenuItem){
+
+        MenuItemCompat.setOnActionExpandListener(searchItem, object: MenuItemCompat.OnActionExpandListener {
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                if (navCtrl.currentDestination!!.id != R.id.searchFragment)
+                    navCtrl.navigate(R.id.searchFragment)
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                navCtrl.navigateUp()
+                return true
+            }
+        })
+    }
+
+    private fun setupBottomNavBar(){
         bottom_navigation.setupWithNavController(navCtrl)
     }
 }
