9d88e1be617bcdcc301a1e6be77560092781ee2d
Milan Kundacina <milankundacina@uns.ac.rs>
2019-03-06 11:50:43 +0100

    Set delay for key not down to 2s, searchview handle empty and back

--- app/src/main/java/com/example/topnews/screens/frame/FrameActivity.kt
+++ app/src/main/java/com/example/topnews/screens/frame/FrameActivity.kt
@@ -2,9 +2,9 @@ package com.example.topnews.screens.frame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.Menu
-import android.view.MenuItem
-import androidx.core.view.MenuItemCompat
+import androidx.appcompat.widget.SearchView
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_frame.*
 class FrameActivity : AppCompatActivity() {
 
     private lateinit var navCtrl: NavController
+    private val waitingTimeForKeyDown = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,29 +28,53 @@ class FrameActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.search, menu)
 
         val searchItem = menu!!.findItem(R.id.search)
-        defineSearchItemBehaviour(searchItem)
+        val searchView = searchItem.actionView as SearchView
+
+        setSearchViewListener(searchView, setUpKeyNotPressedTimer())
 
         return super.onCreateOptionsMenu(menu)
 
     }
 
-    private fun defineSearchItemBehaviour(searchItem: MenuItem){
-
-        MenuItemCompat.setOnActionExpandListener(searchItem, object: MenuItemCompat.OnActionExpandListener {
-            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-                if (navCtrl.currentDestination!!.id != R.id.searchFragment)
-                    navCtrl.navigate(R.id.searchFragment)
-                return true
+    private fun setSearchViewListener(searchView: SearchView, cntrForKeyUp: CountDownTimer) {
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
             }
 
-            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
-                navCtrl.navigateUp()
-                return true
+            override fun onQueryTextChange(newText: String?): Boolean {
+                restartCountdownTimer(cntrForKeyUp)
+                doOnTextChanged(newText!!)
+                return false
             }
+
         })
+
     }
 
-    private fun setupBottomNavBar(){
+    private fun doOnTextChanged(newText: String) {
+        if (navCtrl.currentDestination!!.id != R.id.searchFragment)
+            navCtrl.navigate(R.id.searchFragment)
+        if (newText.isEmpty())
+            navCtrl.navigateUp()
+    }
+
+    private fun restartCountdownTimer(cntrForKeyUP: CountDownTimer) {
+        cntrForKeyUP.cancel()
+        cntrForKeyUP.start()
+    }
+
+    private fun setUpKeyNotPressedTimer(): CountDownTimer {
+        return object : CountDownTimer(waitingTimeForKeyDown.toLong(), 500) {
+            override fun onFinish() {
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+            }
+        }
+    }
+
+    private fun setupBottomNavBar() {
         bottom_navigation.setupWithNavController(navCtrl)
     }
 }
