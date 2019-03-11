efd7ecca40e301ff0067eae9f4aee952eee775f2
Milan Kundacina <milankundacina@uns.ac.rs>
2019-03-11 18:00:43 +0100

    Created wrapped adapter to become generic,made new viewHolders and experimented with generics

--- app/src/main/java/com/example/topnews/screens/readlater/ReadLaterFragment.kt
+++ app/src/main/java/com/example/topnews/screens/readlater/ReadLaterFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -14,11 +13,12 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.topnews.R
 import com.example.topnews.screens.Article
 import com.example.topnews.screens.FakeData
-import com.example.topnews.screens.OnArticleClickListener
+import com.example.topnews.screens.OnRVItemClickListener
+import com.example.topnews.screens.WrappedAdapter
 
 import kotlinx.android.synthetic.main.fragment_read_later.*
 
-class ReadLaterFragment : Fragment(),OnArticleClickListener {
+class ReadLaterFragment : Fragment(), OnRVItemClickListener<Article> {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_read_later, container, false)
@@ -34,31 +34,32 @@ class ReadLaterFragment : Fragment(),OnArticleClickListener {
         readLaterRecyclerView.layoutManager = LinearLayoutManager(context)
         readLaterRecyclerView.adapter = setupReadLaterAdapter()
 
-        val divider = DividerItemDecoration(context,DividerItemDecoration.VERTICAL)
-        divider.setDrawable(getDrawable(context!!,R.drawable.divider)!!)
+        val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        divider.setDrawable(getDrawable(context!!, R.drawable.divider)!!)
         readLaterRecyclerView.addItemDecoration(divider)
     }
 
-    private fun setupReadLaterAdapter(): ReadLaterAdapter {
-        val adapter = ReadLaterAdapter()
-        adapter.setListener(this)
+    private fun setupReadLaterAdapter(): WrappedAdapter<Article> {
+        val adapter = WrappedAdapter<Article>(R.layout.item_read_later, this)
         adapter.setData(FakeData.fetchData())
         return adapter
     }
 
-    override fun articleClicked(dataItem: Article) {
+    override fun itemClicked(dataItem: Article) {
         val bundle = Bundle()
 
-        bundle.putString("urlImg",dataItem.imageUrl)
-        bundle.putString("source",dataItem.source.get("name"))
-        bundle.putString("title",dataItem.title)
-        bundle.putString("description",dataItem.description)
-        bundle.putString("content",dataItem.content)
-        bundle.putString("publishedAt",dataItem.publishedAt)
-        bundle.putString("author",dataItem.author)
-        bundle.putString("urlWeb",dataItem.urlToArticle)
+        bundle.putString("urlImg", dataItem.imageUrl)
+        bundle.putString("source", dataItem.source.get("name"))
+        bundle.putString("title", dataItem.title)
+        bundle.putString("description", dataItem.description)
+        bundle.putString("content", dataItem.content)
+        bundle.putString("publishedAt", dataItem.publishedAt)
+        bundle.putString("author", dataItem.author)
+        bundle.putString("urlWeb", dataItem.urlToArticle)
 
-        Navigation.findNavController(activity!!,R.id.nav_host_fragment).navigate(R.id.action_readLaterFragment_to_articleDetailsFragment,bundle)    }
+        Navigation.findNavController(activity!!, R.id.nav_host_fragment)
+            .navigate(R.id.action_readLaterFragment_to_articleDetailsFragment, bundle)
+    }
 
 
 }
